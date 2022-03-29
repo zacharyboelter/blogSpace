ef5a865baf34276cd5674a06fc755959e6634801
Zachary Boelter <zboelter@gmail.com>
2022-03-29 12:47:46 -0500

    display blog posts

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
-
+let postTitle = document.getElementById('post-title')
+let post = document.getElementById('post-body')
 
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data => {
         const postsArr = data.slice(0, 5)
-        console.log(postsArr)
+        let html = ""
+        for (post of postsArr) {
+            html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            `
+        }
+        document.getElementById('posts').innerHTML = html
     })
