527d1b63c6fcebeade4aa5683e5cc9cf13540e40
Zachary Boelter <zboelter@gmail.com>
2022-03-29 12:58:37 -0500

    for of loop to display all posts

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ let post = document.getElementById('post-body')
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (post of postsArr) {
+        const postArr = data.slice(0, 5)
+        let html = ''
+        for (post of postArr) {
             html += `
             <h3>${post.title}</h3>
             <p>${post.body}</p>
+            <hr />
             `
         }
         document.getElementById('posts').innerHTML = html
